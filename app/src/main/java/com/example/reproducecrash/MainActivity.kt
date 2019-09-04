3d7ee96a1db93a0a0de1446a126876a67d2f6e9d
Cameron Henige <cameronhenige@gmail.com>
2019-09-04 17:04:36 -0400

    first commit

---
+++ app/src/main/java/com/example/reproducecrash/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.reproducecrash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        crash_button.setOnClickListener {
+            FirebaseFirestore.getInstance().collection("Users")
+        }
+    }
+}
